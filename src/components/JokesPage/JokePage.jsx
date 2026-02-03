81e1483a16b8ec22c3beb077bf51b7b10c013af2
Mohsina Parveen <mohsinaparveen@Mohsinas-MacBook-Air.local>
2026-02-03 22:52:21 +0530

    made changes in props

--- src/components/JokesPage/JokePage.jsx
+++ src/components/JokesPage/JokePage.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function JokePage(props) {
+  const [isShown, setIsShown] = useState(false);
+  const [messages, setMessages] = useState(["a", "b", "c"]);
+
+  function toggleShow(props) {
+    setIsShown((prevShown) => !prevShown);
+  }
+  function determineText() {
+    if (messages.length === 0) {
+      return "you are all caught up";
+    } else if (messages.length === 1) {
+      return "you have 1 unread messages";
+    } else {
+      return `you have ${messages.length} messages`;
+    }
+  }
   return (
     <div>
-      <h1>{props.setup}</h1>
-      <p>{props.punchline}</p>
+      {props.setup && <h3>{props.setup}</h3>}
+      {isShown ? <p>{props.punchline}</p> : null}
+      <button onClick={toggleShow}>{isShown ? "hide" : "show"}punchline</button>
+
+      {/*
+      {messages.length === 0 && <h2>no unread messages</h2>}
+
+      {messages.length === 1 && <h3>one unread messages</h3>}
+
+      {messages.length > 1 && (
+        <h3>you have {messages.length} unread messages</h3>
+      )}
+      */}
+
+      <h1>{determineText()}</h1>
     </div>
   );
 }
